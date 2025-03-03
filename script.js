a4de4e9541de4897c96955fabcc1092f3c6283d4
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2025-03-03 21:57:40 +0100

    feat: add CV data JSON file and update contact section in index.html

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 // Mail
-const emailElement = document.getElementById('email');
+const emailElement = document.getElementById("email");
 
-emailElement.addEventListener('click', function() {
+emailElement.addEventListener("click", function () {
   const email = emailElement.textContent;
-  const subject = 'Betreff deiner E-Mail';
-  const body = 'Hier ist der Text deiner E-Mail.';
+  const subject = "Betreff deiner E-Mail";
+  const body = "Hier ist der Text deiner E-Mail.";
   const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-  
+
   window.location.href = mailtoLink;
 });
 
-
 // Interests
-const interHoverElements = document.querySelectorAll('.interHover');
+const interHoverElements = document.querySelectorAll(".interHover");
 
 interHoverElements.forEach((element) => {
-  const icon = element.previousElementSibling; 
+  const icon = element.previousElementSibling;
 
-  icon.addEventListener('mouseenter', () => {
-    element.classList.add('active');
+  icon.addEventListener("mouseenter", () => {
+    element.classList.add("active");
   });
 
-  icon.addEventListener('mouseleave', () => {
-    element.classList.remove('active');
+  icon.addEventListener("mouseleave", () => {
+    element.classList.remove("active");
   });
 });
